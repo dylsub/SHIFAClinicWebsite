c4acab9612745eb8f384980645defa570486c0b6
Dylan Subramanian <dylansubramanian@gmail.com>
2024-10-10 16:35:54 -0500

    Added hello class and about page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="hello">Home Page</h1>
       <h4>Test</h4>
       <p>Hello World</p>
     </div>
