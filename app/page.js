1278413f4e1b844e980830a75d4f2ce4b292ec05
Dylan Subramanian <dylansubramanian@gmail.com>
2024-10-28 21:07:36 -0500

    Minor update to text styling

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Header One</h1>
-      <h2>Header Two</h2>
-      <h3>Header Three</h3>
-      <h4>Header Four</h4>
+      <h1>Header One - h1</h1>
+      <h2>Header Two - h2</h2>
+      <h3>Header Three - h3</h3>
+      <h4>Header Four - h4</h4>
       <p>Paragraph</p>
     </div>
   );
