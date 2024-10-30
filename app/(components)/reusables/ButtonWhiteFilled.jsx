e09e56758186e4b960313fc46f0e074fb82f6852
Faris Siddiqui <faris@siddiqui.md>
2024-10-30 18:42:54 -0500

    Buttons finished!!!

--- app/(components)/reusables/ButtonWhiteFilled.jsx
+++ app/(components)/reusables/ButtonWhiteFilled.jsx
@@ -1,22 +1,11 @@
 import React from "react";
+import Link from "next/link";
+import styles from "./ButtonWhiteFilled.module.css"
 
 export default function ButtonWhiteFilled({ children, path }) {
-  // Faris + Devin
-
-  // Create a button that resembles the white button with black text seen on the home page with text "View Services"
-  // Give it a border-radius, border-width, border-color of white, text of black
-  // Make it dynamic. It should scale to fit the text written.
-  // Text should be centered
-
-  // IMPORTANT: Please use a nextJS <Link> tag instead of a <button> element.
-
-  // Huge hint:
-  //   <Link href="/about" passHref>
-  //     <a style={buttonStyle}>Go to About Page</a>
-  //   </Link>;
-
-  // Text will be automatically entered in its children property. The path parameter is the route that the button should link to.
-  // CSS should be done in ButtonWhiteFilled.module.css for right now. Please use detailed classNames to avoid overlap
-
-  return <div>{children}</div>;
+  return (
+    <Link href={path} className={styles.button}>
+      {children}
+    </Link>
+    );
 }
--- app/(components)/reusables/ButtonWhiteTransparent.jsx
+++ app/(components)/reusables/ButtonWhiteTransparent.jsx
@@ -1,22 +1,11 @@
 import React from "react";
+import Link from "next/link";
+import styles from "./ButtonWhiteTransparent.module.css"
 
 export default function ButtonWhiteTransparent({ children, path }) {
-  // Faris + Devin
-
-  // Create a button that resembles the white button with a transparent background seen on the home page with text "Request Appointment"
-  // Give it a border-radius, border-width, border-color of white, text of white, and background of transparent
-  // Make it dynamic. It should scale to fit the text written.
-  // Text should be centered
-
-  // IMPORTANT: Please use a nextJS <Link> tag instead of a <button> element.
-
-  // Huge hint:
-  //   <Link href="/about" passHref>
-  //     <a style={buttonStyle}>Go to About Page</a>
-  //   </Link>;
-
-  // Text will be automatically entered in its children property. The path parameter is the route that the button should link to.
-  // CSS should be done in ButtonWhiteTransparent.module.css for right now. Please use detailed classNames to avoid overlap
-
-  return <div>{children}</div>;
+  return (
+    <Link href={path} className={styles.button}>
+      {children}
+    </Link>
+    );
 }
