a8a40e088b7dfea052820b9b0328cb9528912279
ashleet2 <ashleet2@illinois.edu>
2024-10-31 17:54:04 -0500

    Added home hero

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import HomeHero from "./(components)/home/HomeHero";
 
 export default function Home() {
   return (
     <div>
+      <HomeHero></HomeHero>
       <h1>Header One - h1</h1>
       <h2>Header Two - h2</h2>
       <h3>Header Three - h3</h3>
