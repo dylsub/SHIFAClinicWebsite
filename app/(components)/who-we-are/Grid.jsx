f1ab44c01e1b956a489b15046df01ddf227b8ba8
Dylan Subramanian <dylansubramanian@gmail.com>
2024-10-31 14:36:28 -0500

    Added a few deliverables

---
+++ app/(components)/who-we-are/Grid.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Grid = () => {
+  // Ayush + Ryan
+  // Using the GridText and GridImage components, construct the mission/vision section in its entirety
+  // Make sure that it fills the whole width of the screen
+
+  return <div></div>;
+};
+
+export default Grid;
---
+++ app/(components)/who-we-are/GridImage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GridImage = () => {
+  // Ayush + Ryan
+  // Look at the mission and vision section of the Who We Are page
+  // This will be a container holding an image with a width and height of 50vw (square)
+  // Make sure that this crops the image and does not scsale it
+  // Make sure the image is centered in the box before cropped
+
+  return <div></div>;
+};
+
+export default GridImage;
