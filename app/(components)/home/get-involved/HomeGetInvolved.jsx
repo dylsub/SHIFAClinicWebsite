0501bee3d281bb9a9f23c176008a3b52919aaa8d
Dylan Subramanian <dylansubramanian@gmail.com>
2024-10-31 01:34:09 -0500

    Added some more deliverables

---
+++ app/(components)/home/get-involved/HomeGetInvolved.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeGetInvolved = () => {
+  // Armaan
+  // Now that you have the detail text block component created,
+  // You want to design the home get involved section.
+  // The section be held in a content box (div) with a width of 1400px
+  // An image and your created component should be in those boxes and styled appropriately
+  return <div></div>;
+};
+
+export default HomeGetInvolved;
