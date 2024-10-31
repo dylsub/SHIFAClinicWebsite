f94cc47e09ea626ad1ee9c41f9d8020bd59d5033
ashleet2 <ashleet2@illinois.edu>
2024-10-31 17:46:50 -0500

    Added home hero section

--- app/(components)/home/HomeHero.jsx
+++ app/(components)/home/HomeHero.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import styles from "./HomeHero.module.css";
+import ButtonWhiteTransparent from "../reusables/ButtonWhiteTransparent";
+import ButtonWhiteFilled from "../reusables/ButtonWhiteFilled";
+
 
 export default function HomeHero() {
-  // Ashlee Tran + Victor 
+ // Ashlee Tran + Victor
 
-  // Hero Image (Gradient black to clear from bottom to top)
-  // Stretchs 100vw and a constant pixel height
 
-  // h1 header with a margin left of around 10-15vw
-  // Include placeholders for buttons (use default button). Someone else will design those.
+ // Hero Image (Gradient black to clear from bottom to top)
+ // Stretchs 100vw and a constant pixel height
 
-  // Style in HomeHero.module.css for right now
 
-  return (
-    <div className={styles.heroContainer}>
-      <div className={styles.overlay}></div>
-      <h1 className={styles.heroHeader}>Sacred healing for all</h1>
-        <button className={styles.heroButton1}>Request appointment</button>
-        <button className={styles.heroButton2}>View services</button>
-      {/* <img src="./Control-V.png"></img> */}
-    </div>
-  );
+ // h1 header with a margin left of around 10-15vw
+ // Include placeholders for buttons (use default button). Someone else will design those.
+
+
+ // Style in HomeHero.module.css for right now
+
+
+ return (
+   <div className={styles.heroContainer}>
+     {/* <div className={styles.overlay}></div> */}
+     <div className={styles.text_box}>
+       <h1 className={styles.heroHeader}>Sacred healing for all</h1>
+       <ButtonWhiteTransparent path="/">Request Appointment</ButtonWhiteTransparent>
+       <ButtonWhiteFilled path="/">View services</ButtonWhiteFilled>
+     </div>
+  
+
+
+   </div>
+ );
+
 
 }
