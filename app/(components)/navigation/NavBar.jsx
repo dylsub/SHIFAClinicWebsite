5ff1844fc74274d319a9cb974a85fccc3b7d03bb
ayushshastry <shastryayush@gmail.com>
2024-10-31 12:55:37 -0500

    navbar-ayush/gao

--- app/(components)/navigation/NavBar.jsx
+++ app/(components)/navigation/NavBar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Link from 'next/link';
+import Image from "next/image";
+import styles from './NavBar.module.css'
 
 export default function NavBar() {
   // Ayush Shastry + Ryan Gao
@@ -17,5 +20,25 @@ export default function NavBar() {
 
   // Style in the NavBar.module.css file for right now
 
-  return <div></div>;
+  return (
+    <nav className={styles.navbar}>
+      {/* Logo Section */}
+      <div className={styles.logo}>
+        <Link href="/"> <Image src="/shifa.png" alt = "Logo" width={40} height={40}/> </Link>
+      </div>
+
+      {/* Navigation Links Section */}
+      <div className={styles.navLinks}>
+        <Link href="/" className={styles.link}>Home</Link>
+        <Link href="/" className={styles.link}>About</Link>
+        <Link href="/" className={styles.link}>Services</Link>
+      </div>
+
+      {/* Buttons Section */}
+      <div className={styles.buttons}>
+        <Link href="/" className={styles.button}>Sign Up</Link>
+        <Link href="/" className={styles.button}>Login</Link>
+      </div>
+    </nav>
+  );
 }
