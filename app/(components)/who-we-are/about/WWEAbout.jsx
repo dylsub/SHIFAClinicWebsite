bcb49f13a48e247a8bdada46a9e4fd6c5dbb4229
swethay24 <swethay2@illinois.edu>
2024-11-07 05:24:08 -0600

    added wwe about section

--- app/(components)/who-we-are/about/WWEAbout.jsx
+++ app/(components)/who-we-are/about/WWEAbout.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import ButtonBlackTransparent from "../../reusables/ButtonBlackTransparent";
+
+// I cooked
 
 const WWEAbout = () => {
-  // Swetha
-  // Create the who are we about section ("Serving Humanity without Borders")
-  // Text should be centered (h1 and p)
-  // Text should be held in a div content box with a width of 1400px.
-  return <div></div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: "100px 320px", backgroundColor: "#f1f1f1" }}>
+      <div style={{ maxWidth: "1400px", display: "flex", alignItems: "center"}}>
+        
+        
+        <div>
+          <p style={{ width: "350px", fontSize: "56px", lineHeight: "1", fontFamily: "Inter" }}>  
+            We serve<br /> humanity<br /> without any<br /> boundaries
+          </p>
+        </div>
+
+        
+        <div>
+          <p style={{ fontSize: "20px", lineHeight: "1.5", fontFamily: "Inter", paddingBottom: "20px" }}>
+            ICN SHIFA Clinic is a manifestation of a desire to serve humanity without any boundaries. 
+            Since being founded in 2017, the clinic has proudly transformed into a new ICN SHIFA Clinic 
+            in a newly built dedicated office space with a team of almost 50 volunteers, offering 
+            several services to its patients at no cost.
+          </p>
+          
+          <ButtonBlackTransparent path="/">Our Team</ButtonBlackTransparent>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 
 export default WWEAbout;
