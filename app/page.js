3b8a062abf2fd0a1446897c70ca770e13bc98409
armaanpatel27 <armaan0427@gmail.com>
2024-11-21 15:40:41 -0600

    Changes week 6

--- app/page.js
+++ app/page.js
@@ -8,6 +8,8 @@ import Grid from "./(components)/who-we-are/grid/Grid";
 import HomeHero from "./(components)/home/HomeHero";
 import WWAHero from "./(components)/who-we-are/WWAHero";
 import HomeAbout from "./(components)/home/about/HomeAbout";
+import HomeGetInvolved from "./(components)/home/get-involved/HomeGetInvolved";
+import DonateThanks from "./(components)/donate/DonateThanks";
 
 export default function Home() {
   return (
@@ -17,6 +19,13 @@ export default function Home() {
       <WWAHero></WWAHero>
 
       <HomeHero></HomeHero>
+      <HomeAbout subtitleText={"Non-profit medical clinic"}
+      bodyText={"ICN SHIFA Clinic is a manifestation of a desire to serve humanity without any boundaries. ICN Free Clinic was started in its rudimentary form as a makeshift clinic in the hallways of Islamic Center of Naperville with a small team of passionate doctors."}
+      buttonText={"Who we are"}
+      path={"/"}
+      src={"home_about.png"}
+      headerText={"Uplifting the community with health services"}
+      ></HomeAbout>
 
       <WWAAbout></WWAAbout>
 
@@ -40,7 +49,22 @@ export default function Home() {
       ></HomeServicesPanel>
 
       <Statistics></Statistics>
-
+      
+      <HomeGetInvolved subtitleText={"Join our team"}
+      bodyText={"The ICN SHIFA Clinic is looking for new volunteers to join our community. We offer opportunities ranging from job shadowing to full-time volunteering positions. We’d love to have you!"}
+      buttonText={"Learn More"}
+      path={"/"}
+      src={"join_our_team.png"}
+      headerText={"Get Involved"}
+      ></HomeGetInvolved>
+      
+      <DonateThanks subtitleText={""}
+      bodyText={"Your generosity helps us continue providing free medical services to those in need, thanks to the dedication of our incredible volunteer team. Every contribution, no matter the amount, supports our mission to offer compassionate care to the community. With your support, we can make a meaningful difference in the health and well-being of individuals and families in need."}
+      buttonText={"Ways to Donate"}
+      path={"/"}
+      src={"nurse-treating-patient-stockcake.png"}
+      headerText={"Thank you for considering a donation to ICN SHIFA Clinic!"}
+      ></DonateThanks>
       <h1>Header One - h1</h1>
       <h2>Header Two - h2</h2>
       <h3>Header Three - h3</h3>
