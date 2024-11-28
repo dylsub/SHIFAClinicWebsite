56cf8f3c8ed0fb4a9b3e882ccb1c3f63c96ca4aa
Dylan Subramanian <dylansubramanian@gmail.com>
2024-11-28 15:52:09 -0600

    Setup who we are page and added responsiveness

--- app/(components)/who-we-are/WWAHero.jsx
+++ app/(components)/who-we-are/WWAHero.jsx
@@ -3,7 +3,6 @@ import styles from "./WWAHero.module.css";
 import ButtonAnyColorFilled from "../reusables/buttons/ButtonAnyColorFilled";
 import ButtonWhiteFilled from "../reusables/buttons/ButtonWhiteFilled";
 
-
 const WWAHero = () => {
   // Ashlee + Victor
   // Do the same thing as the home hero
@@ -13,16 +12,17 @@ const WWAHero = () => {
   return (
     <div className={styles.WWAContainer}>
       {/* <div className={styles.overlay}></div> */}
-      <div className={styles.text_box}>
-        <h1 className={styles.WWAHeader}>Who we are</h1>
-        <div className={styles.Buttons} >
-          <ButtonAnyColorFilled path="/" buttonColor="rgb(255, 0, 0)">Our Mission and Vision</ButtonAnyColorFilled>
-          <ButtonWhiteFilled path="/">Our Team</ButtonWhiteFilled>
+      <div className={styles.text_box + " content_box"}>
+        <div>
+          <h1 className={styles.WWAHeader}>Who we are</h1>
+          <div className={styles.button_box}>
+            <ButtonAnyColorFilled path="/" buttonColor="rgb(255, 0, 0)">
+              Our Mission and Vision
+            </ButtonAnyColorFilled>
+            <ButtonWhiteFilled path="/">Our Team</ButtonWhiteFilled>
+          </div>
         </div>
       </div>
-   
- 
- 
     </div>
   );
 };
--- app/(components)/who-we-are/grid/Grid.jsx
+++ app/(components)/who-we-are/grid/Grid.jsx
@@ -1,16 +1,31 @@
+"use client";
+
 import React from "react";
 import GridText from "./GridText";
 import GridImage from "./GridImage";
 import styles from "./Grid.module.css";
 
 const Grid = () => {
+  const isMobile = window.innerWidth <= 720;
   return (
     <div className={styles.container}>
-      <GridImage src="/doctors.jpg" alt="Doctors" />
-      <GridText
-        title="Mission"
-        text="ICN SHIFA Clinic aims to provide free quality healthcare to anyone without insurance or underinsured, regardless of race, religion, or political affiliation."
-      />
+      {!isMobile ? (
+        <>
+          <GridImage src="/doctors.jpg" alt="Doctors" />
+          <GridText
+            title="Mission"
+            text="ICN SHIFA Clinic aims to provide free quality healthcare to anyone without insurance or underinsured, regardless of race, religion, or political affiliation."
+          />
+        </>
+      ) : (
+        <>
+          <GridText
+            title="Mission"
+            text="ICN SHIFA Clinic aims to provide free quality healthcare to anyone without insurance or underinsured, regardless of race, religion, or political affiliation."
+          />
+          <GridImage src="/doctors.jpg" alt="Doctors" />
+        </>
+      )}
       <GridText
         title="Vision"
         text="ICN SHIFA Clinic has a vision for a facility that will offer comprehensive healthcare services under one roof, including medical, mental, dental, optical, and specialty care. We aim to create a system where patients without health insurance benefit from cost-effective, patient-focused services."
