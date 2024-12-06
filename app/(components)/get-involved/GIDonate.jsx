c886bd2a036668d91704535795c13b80a68bf78c
swethay24 <swethay2@illinois.edu>
2024-12-05 19:35:32 -0600

    fixed gidonate and did ramorequestions

--- app/(components)/get-involved/GIDonate.jsx
+++ app/(components)/get-involved/GIDonate.jsx
@@ -5,31 +5,23 @@ import classes from "./GIDonate.module.css";
 const GIDonate = () => {
   return (
     <div className={classes.container}>
-      <h1
-        style={{
-          fontSize: "36px",
-          marginBottom: "30px",
-        }}
-      >
+      <h1>
         Your Support Matters
       </h1>
-      <p
-        style={{
-          fontSize: "18px",
-          lineHeight: "1.6",
-          maxWidth: "1200px",
-          marginBottom: "45px",
-        }}
-      >
+      <p>
         Giving back to the community is a principle we value, and donations from
         our community are greatly appreciated. Your generous donations are vital
         in helping us provide essential healthcare services. Thank you for your
         generosity and helping us create a difference for those in need!
       </p>
-
+      
+      <div className={classes.buttonContainer}>
       <ButtonAnyColorFilled path="/donate" buttonColor="rgb(0, 0, 0)">
         Donate Today!
       </ButtonAnyColorFilled>
+      </div>
+      
+
     </div>
   );
 };
--- app/(components)/request-appointment/RAMoreQuestions.jsx
+++ app/(components)/request-appointment/RAMoreQuestions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from "./RAMoreQuestions.module.css";
 
 const RAMoreQuestions = () => {
   // Swetha
@@ -7,7 +8,20 @@ const RAMoreQuestions = () => {
   // Simple header but then you have two red filled buttons connected with a line
   // Make sure that it is stored in a content box just like we have been doing
 
-  return <div></div>;
+  return (
+    <div className={classes.contentBox}>
+      <h2 className={classes.header}>Have More Questions About Your Health?</h2>
+      <div className={classes.buttonContainer}>
+        <button className={`${classes.redButton} ${classes.leftButton}`}>
+          Request an Appointment
+        </button>
+        <div className={classes.line}></div>
+        <button className={`${classes.redButton} ${classes.rightButton}`}>
+          Ask a SHIFA Doctor!
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RAMoreQuestions;
