7d2b88240e0847454d5368e189a0b200c247ce2a
Ishaan Bhandari <ishaanbhandari0@gmail.com>
2025-01-07 21:58:25 -0800

    finished why donate and volunteer testimonial

--- app/(components)/get-involved/VolunteerTestimonial.jsx
+++ app/(components)/get-involved/VolunteerTestimonial.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import styles from "./VolunteerTestimonial.module.css";
 
-const VolunteerTestimonial = () => {
-  // Ishaan
-  // Use the InfoGroup.jsx/InfoPanel.jsx components you created to set up the "Hear from our volunteers"
-  // Add the title text and button
-  // Style accordingly
-
-  return <div></div>;
-};
-
-export default VolunteerTestimonial;
+export default function VolunteerTestimonial({ children }) {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.heading}>Hear from our volunteers!</h2>
+      {children}
+      <button className={styles.button}>Tell Us About Your Experience!</button>
+    </div>
+  );
+}
